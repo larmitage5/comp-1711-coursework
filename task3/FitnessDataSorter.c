48720636aee20b4c9a953d7e409968c32362fad3
larmitage5 <146672196+larmitage5@users.noreply.github.com>
2023-12-02 14:50:27 +0000

    add

--- task3/FitnessDataSorter.c
+++ task3/FitnessDataSorter.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 
 // Define an appropriate struct
@@ -8,7 +9,7 @@ typedef struct {
 	char date[11];
 	char time[6];
 	int steps;
-} FITNESS_DATA;
+} FitnessData;
 
 // Define any additional variables here
 /**
@@ -49,28 +50,7 @@ void tokeniseRecord(const char *input, const char *delimiter,
 
 // Complete the main function
 int main() {
-    FITNESS_DATA data[100];
-    int buffer_size = 250;
-    char line[buffer_size];
-    int counter=0;
-
-    FILE *input = fopen("FitnessData_2023.csv", "r");
-
-    char date[100];
-    char time[100];
-    char steps[100];
-
-    while (fgets(line, buffer_size, input))
-    {   
-        tokeniseRecord(line,",",date,time,steps);
-        strcpy(data[counter].date,date);
-        strcpy(data[counter].time,time);
-        data[counter].steps = atoi(steps);
-        counter++;
-    }
-
-int main() {
-    FitnessData data[100];
+    FitnessData data[300];
     int buffer_size = 250;
     char line[buffer_size];
     char filename[buffer_size];
@@ -78,17 +58,9 @@ int main() {
     char date[100];
     char time[100];
     char steps[100];
+    int i;
 
-    while (fgets(line, buffer_size, input))
-    {   
-        tokeniseRecord(line,",",date,time,steps);
-        strcpy(data[counter].date,date);
-        strcpy(data[counter].time,time);
-        data[counter].steps = atoi(steps);
-        counter++;
-    }
-
-    printf("Enter Filename:");
+    printf("Enter Filename: ");
     fgets(line, buffer_size, stdin);
     sscanf(line, " %s ", filename);
     FILE *input = fopen(filename, "r");
@@ -98,27 +70,55 @@ int main() {
         return 1;
     }
 
-    
+    while (fgets(line, buffer_size, input))
+    {   
+        tokeniseRecord(line,",",date,time,steps);
+        strcpy(data[counter].date,date);
+        strcpy(data[counter].time,time);
+        data[counter].steps = atoi(steps);
+        counter++;
+    }
+
+    for (i=0;i<counter;i++){
+        if(strlen(data[i].date) != 10){
+            printf("Error: incorrect data\n");
+            return 1;
+        }
+        if (strlen(data[i].time) != 5 ){
+            printf("Error: incorrect data\n");
+            return 1;
+        }
+        if (isdigit(data[i].steps) == 1){
+            printf("Error: incorrect data\n");
+            return 1;
+        }
+    }
+    printf("File opened successfully\n");
+
     int arr[counter];
     int max = 0;
     int a = 0;
-    int i;
 
     for (i = 0;i<counter;i++){
-        if (data[i].steps>max){
-            max = data[i].steps;
-            arr[a] = i;
-            a += 1;
+        for (int j = i + 1; j<counter;j++){
+            if(arr[i]<arr[j]){
+                int k = arr[i];
+                arr[i] = arr[j];
+                arr[j] = k;
+            }
         }
     }
 
-    FILE *file = fopen(filename.tsv, "r+");
+    FILE *file = fopen("FitnessData_2023.csv.tsv", "a+");
     if (file == NULL) {
         perror("");
         return 1;
     }
     for (i=0;i<counter;i++){
-        fprintf("%s\t%s\t%d", data[arr[i]].date, data[arr[i]].time, data[arr[i]].steps);
+        fprintf(file, "%s\t%s\t%d", data[arr[i]].date, data[arr[i]].time, data[arr[i]].steps);
+        printf("%s\t%s\t%d", data[arr[i]].date, data[arr[i]].time, data[arr[i]].steps);
     }
+    fclose(input);
+    fclose(file);
     return 0;
 }
